1d126a5bb207365a392b2a8baca408fe6b21463e
Karen Wafik <karenwafik113@gmail.com>
2026-06-29 23:05:33 +0300

    update music

--- Invetion.js
+++ Invetion.js
@@ -1,25 +1,52 @@
+// function openInvitation() {
+//     window.location.href = "details.html";
+// }
+// const button = document.querySelector(".open-btn");
+// const music = document.getElementById("bgMusic");
+
+// button.addEventListener("click", function () {
+
+//     music.addEventListener('loadedmetadata', function () {
+//         music.currentTime = 15;
+//         music.play();
+//     });
+
+//     if (music.readyState >= 1) {
+//         music.currentTime = 15;
+//         music.play();
+//     }
+
+//     setTimeout(() => {
+//         window.location.href = "details.html";
+//     }, 1000);
+
+
+
+// });
+
+
+
 function openInvitation() {
     window.location.href = "details.html";
+    const music = document.getElementById("bgMusic");
+
+    // تشغيل الموسيقى من الثانية 15
+    music.currentTime = 15;
+
+    music.play()
+        .then(() => {
+            // حفظ حالة التشغيل
+            sessionStorage.setItem("playMusic", "true");
+
+            // الانتقال بعد ثانية
+            setTimeout(() => {
+                window.location.href = "details.html";
+            }, 1000);
+        })
+        .catch((error) => {
+            console.log("Audio could not play:", error);
+
+            // الانتقال حتى لو المتصفح منع تشغيل الصوت
+            window.location.href = "details.html";
+        });
 }
-const button = document.querySelector(".open-btn");
-const music = document.getElementById("bgMusic");
-
-button.addEventListener("click", function () {
-
-    music.addEventListener('loadedmetadata', function () {
-        music.currentTime = 15;
-        music.play();
-    });
-
-    if (music.readyState >= 1) {
-        music.currentTime = 15;
-        music.play();
-    }
-
-    setTimeout(() => {
-        window.location.href = "details.html";
-    }, 1000);
-
-
-
-});
